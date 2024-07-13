ea0760847ab7c07ed8a4bcc7f3e6357a81003559
ioannis <IoannisTselekoglou@rwth-aachen.deexit>
2024-07-13 23:31:14 +0200

    Changing filename

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import pandas as pd
+iport pandas as pd
 import numpy as np 
-from calculation import Smootherpipe as sp
+
+from smootherpipe import Smootherpipe as sp
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
 
+    x = np.linspace(-10,10,300)
+    y_true = np.sin(x)
+    noise = 0.9 * np.random.normal(size=len(x))
+    y_noise = y_true + noise
+    y_smoothed = sp.convolution2D(y_noise, kernel_size=10)
 
-    test = [1,3,4,2,13,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4,2,4]
 
-    #smoother = ca()
-    #print(smoother.convolution2D(test, kernel_size=10))
+    #Plotting 
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y_noise, label='Noisy data', color='blue', marker='o')
+    plt.scatter(x, y_smoothed, label='Smoothed data', color='red', marker = 'o')
+    plt.title('Sinc Curve with Added Noise')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
 
-    print(sp.convolution2D(test,kernel_size=10))
